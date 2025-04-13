1041740de24a2dc20e5382f720c4c316395d8cc3
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2025-04-13 11:31:49 -0500

    practiced contains duplicate

--- containsDuplicate/containsDuplicate.js
+++ containsDuplicate/containsDuplicate.js
@@ -1,53 +1,14 @@
-// class Solution {
-//     /**
-//      * @param {number[]} nums
-//      * @return {boolean}
-//      */
-//     hasDuplicate(nums) {
-//         for (let i = 0; i < nums.length; i++) {
-//             for (let j = i + 1; j < nums.length; j++) {
-//                 if (nums[i] === nums[j]) {
-//                     return true;
-//                 }
-//             }
-//         }
-//         return false;
-//     }
-// }
+//brute force, sorting method, hash set method
 
 class Solution {
-    /**
-     * @param {number[]} nums
-     * @return {boolean}
-     */
-    hasDuplicate(nums) {
-        nums.sort((a, b) => a - b);
-        for (let i = 1; i < nums.length; i++) {
-            console.log(nums[i], nums[i - 1])
-            if (nums[i] === nums[i - 1]) {
-                return true;
-            }
-        }
-        return false;
-    }
+	hasDuplicate(nums) {
+	}
 }
 
-class Solution {
-    hasDuplicate(nums) {
-        const seen = new Set()
+const test = new Solution();
 
-        for (const num of nums) {
-            if (seen.has(num)) {
-                return true;
-            }
-            seen.add(num);
-        }
+console.log(test.hasDuplicate([1, 2, 3, 3])); //true
+console.log(test.hasDuplicate([1, 2, 3, 4])); //false
+console.log(test.hasDuplicate([1, 2, 3, 4, 5, 1, 23, 5])); // true
+console.log(test.hasDuplicate([1, 2, 3, 4, 99, 0, -1, 5])); //false
 
-        return false;
-    }
-}
-
-const test = new Solution()
-
-console.log(test.hasDuplicate([1,2,3,3]))
-console.log(test.hasDuplicate([1,2,3,4]))
